19a628ce2e781bd7e605add1b05c738c5f3f7f04
vv198x <vv198x@gmail.com>
2022-12-08 22:38:26 +0500

    add white list

--- deploy/send.sh
+++ deploy/send.sh
@@ -4,8 +4,7 @@ GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o deploy/go2ban
 cd deploy
 echo -install
 scp go2ban n:/root/go2ban/
-if [ "-install" = "$1" ]
-then
+if [ "-install" = "$1" ]; then
   ssh n mkdir /root/go2ban
   scp install.sh n:/root/go2ban/
   scp go2ban.conf n:/root/go2ban/
--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -2,21 +2,29 @@ package validator
 
 import (
 	"errors"
+	"go2ban/pkg/config"
 	"go2ban/pkg/osUtil"
 	"regexp"
 )
 
 func CheckIp(target string) (end string, err error) {
-	localAddress := osUtil.GetLocalIPs()
 	target = regexp.MustCompile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}`).FindString(target)
 	if target != "" {
-		for _, addr := range localAddress {
+		whiteAddress := osUtil.GetLocalIPs()
+		whiteAddress = append(whiteAddress, config.Get().WhiteList...)
+		err = errors.New("This is white ip: " + target)
+		for _, addr := range whiteAddress {
+			if addr[len(addr)-1] == '*' {
+				rexp := regexp.MustCompile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?){3}`)
+				if rexp.FindString(addr) == rexp.FindString(target) {
+					return "", err
+				}
+			}
 			if target == addr {
-				return "", errors.New("This is local ip: " + target)
+				return "", err
 			}
 		}
-		//for i, addr := range //TODO White list
 		return target, nil
 	}
-	return "", errors.New("Wrong ip: " + target)
+	return "", errors.New("Wrong ip")
 }
