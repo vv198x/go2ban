c4776e15e332c57042610001dffd9eddb7a9ae39
vv198x <vv198x@gmail.com>
2023-02-13 15:14:42 +0500

    add test

--- main_test.go
+++ main_test.go
@@ -106,7 +106,7 @@ func TestStartMain(t *testing.T) {
 	}
 	defer file.Close()
 
-	_, err = file.Write([]byte(`firewall=auto`))
+	_, err = file.Write([]byte(`firewall=iptables`))
 	if err != nil {
 		t.Fatalf("Error writing test config file: %v", err)
 	}
