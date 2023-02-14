45daa540d0ca95ed06994d0ff397f79e631c279e
vv198x <vv198x@gmail.com>
2023-02-14 16:15:37 +0500

    change tests

--- api/grpc/server_test.go
+++ api/grpc/server_test.go
@@ -1,7 +1,12 @@
 package grpc
 
 import (
+	"bytes"
+	"github.com/vv198x/go2ban/config"
 	"google.golang.org/grpc"
+	"log"
+	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -31,4 +36,37 @@ func TestStart(t *testing.T) {
 			time.Sleep(time.Millisecond * 100)
 		})
 	}
+}
+
+func TestStart2(t *testing.T) {
+	// Test case 1: runAsDaemon=false
+	Start(false) // Expect no action
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer log.SetOutput(os.Stderr)
+
+	// Test case 2: valid grpc port configuration and runAsDaemon=true
+	config.Get().GrpcPort = "8080/tcp"
+	runAsDaemon := true
+	Start(runAsDaemon)
+	time.Sleep(100 * time.Millisecond) // wait for goroutine to start
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		t.Errorf("Failed to dial gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	// Test case 3: invalid grpc port configuration and runAsDaemon=true
+	config.Get().GrpcPort = "8080" // invalid port format
+	Start(runAsDaemon)
+	time.Sleep(100 * time.Millisecond) // wait for goroutine to start
+	// Expect error message logged to console
+
+	Start(runAsDaemon)
+
+	if strings.Contains(buf.String(), "gRPC error") {
+		t.Errorf("Expected error message for invalid port configuration")
+	}
+
 }
